190f55e2bb8faa169682fb7322fe3bf9bac29694
Assapatt <2426864618@qq.com>
2019-11-04 16:53:48 +0800

    cnode

---
+++ cnode/precache-manifest.3dbf1d618fce095d9ed6ea01ffe4b70a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57abcdcabafd7a152405fd548ff050f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68826cbacded587bf82f",
+    "url": "/static/css/2.aa7fcd32.chunk.css"
+  },
+  {
+    "revision": "9f6242adf48d73cc57cd",
+    "url": "/static/css/main.c4b5494b.chunk.css"
+  },
+  {
+    "revision": "68826cbacded587bf82f",
+    "url": "/static/js/2.aedaab30.chunk.js"
+  },
+  {
+    "revision": "9f6242adf48d73cc57cd",
+    "url": "/static/js/main.63e88994.chunk.js"
+  },
+  {
+    "revision": "1127e2a26bc7f540fb90",
+    "url": "/static/js/runtime-main.73267343.js"
+  }
+]);
