252b6b641919d6f716c4b36f33b1f01c83451a63
Assapatt <2426864618@qq.com>
2019-11-14 17:09:56 +0800

    zhuba app

---
+++ 住吧/precache-manifest.91ccaf04526f2c2b75c1df0fa58214e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d586d108dca3c8de83350304f32df16c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f229890020021a83e727",
+    "url": "./static/css/2.6f60e7f3.chunk.css"
+  },
+  {
+    "revision": "7913d42a69b92840c28e",
+    "url": "./static/css/main.2680dfa7.chunk.css"
+  },
+  {
+    "revision": "f229890020021a83e727",
+    "url": "./static/js/2.26972a4b.chunk.js"
+  },
+  {
+    "revision": "7913d42a69b92840c28e",
+    "url": "./static/js/main.53782000.chunk.js"
+  },
+  {
+    "revision": "b4975b401303f1e5c73b",
+    "url": "./static/js/runtime-main.3a5a28a7.js"
+  }
+]);
